d169a5603e54230ab2c9af140a3278f35b3271b7
maks-vynnychenko <vinnichenkomaks@gmail.com>
2023-05-27 20:45:08 +0300

    Update gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+
+const galerryMarkup = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class='gallery__item'>
+      <a class='gallery__link' href='${original}'>
+      <img
+        class='gallery__image'
+        src='${preview}'
+        alt='${description}'/>
+      </a>
+      </li>`
+);
+
+gallery.insertAdjacentHTML('beforeend', galerryMarkup.join(''));
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
 
 console.log(galleryItems);
