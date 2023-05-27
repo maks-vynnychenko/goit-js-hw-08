81c419612062754d95f94569d83991f293006c07
maks-vynnychenko <vinnichenkomaks@gmail.com>
2023-05-28 00:55:16 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,3 +2,33 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onInputData), 500);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarger.reset();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  localStorage.removeItem(STORAGE_KEY);
+}
+function onInputData(event) {
+  let inputData = localStorage.getItem(STORAGE_KEY);
+  inputData = inputData ? JSON.parse(inputData) : {};
+  inputData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(inputData));
+}
+
+function outputData() {
+  let outputData = localStorage.getItem(STORAGE_KEY);
+  try {
+    if (outputData) {
+      outputData = JSON.parse(outputData);
+      Object.entries(outputData).forEach(([name, value]) => {
+        form.elements[name].value = value;
+      });
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+}
